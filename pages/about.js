c5bfcfd95897e4555b1967287a650391b097c541
MiguelRosasVillalobos <miguel.rosas2001@alumnos.ubiobio.cl>
2024-04-14 17:59:16 -0400

    se sube icon y modifica about

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,12 @@
+import Link from 'next/link';
+
 export default function About() {
   return (
     <div>
       <h1>About</h1>
       <p>This is the about page</p>
       <h2>
-        <Link href="/index">Back to home</Link>
+        <Link href="/">Back to home</Link>
       </h2>
     </div>
   );
