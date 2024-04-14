7b10c68fde71c216507d3fd4bb78703fd68d9c30
MiguelRosasVillalobos <miguel.rosas2001@alumnos.ubiobio.cl>
2024-04-14 17:42:19 -0400

    se sube about

--- pages/about.js
+++ pages/about.js
@@ -3,6 +3,9 @@ export default function About() {
     <div>
       <h1>About</h1>
       <p>This is the about page</p>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
     </div>
   );
   }
